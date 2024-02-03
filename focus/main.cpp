290ba60d198f5010f7401f10ccdcf7ebded22896
K4HVH <bronercast@gmail.com>
2024-02-03 22:53:41 +1000

    Gui with imguibundle

--- focus/main.cpp
+++ focus/main.cpp
@@ -3,20 +3,38 @@
 
 #include "mouse_driver/mouse.hpp"
 
+#include "hello_imgui/hello_imgui.h"
+#include "imgui.h"
+#include "imgui_internal.h"
+
+void Gui()
+{
+	ImGui::Text("Hello");
+	ImGui::ShowIDStackToolWindow();
+}
+
+
 int main()
 {	
+	HelloImGui::RunnerParams runnerParams;
+	runnerParams.callbacks.ShowGui = Gui;
 
-	if (!mouse_open()) {
-		printf("[-] failed to open ghub macro driver\n");
-		return 0;
-	}
+	//runnerParams.useImGuiTestEngine = true;
 
-	for (int i = 0; i < 32; i++) {
-		Sleep(100);
-		printf("[+] moving mouse\n");
+	HelloImGui::Run(runnerParams);
+	return 0;
 
-		mouse_move(0, -10, 0, 0);
-	}
+	//if (!mouse_open()) {
+	//	printf("[-] failed to open ghub macro driver\n");
+	//	return 0;
+	//}
 
-	mouse_close();
+	//for (int i = 0; i < 32; i++) {
+	//	Sleep(100);
+	//	printf("[+] moving mouse\n");
+
+	//	mouse_move(0, -10, 0, 0);
+	//}
+
+	//mouse_close();
 }
